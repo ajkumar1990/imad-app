b025d93d0670c69d8bb3513a0abaea07a99c697f
Ajay Kumar <ajkumar1990@gmail.com>
2017-08-10 20:41:47 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 // Move the image
-var img = document.getElementById('madi');
+var img = document.getElementById('AJ');
 var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 1;
